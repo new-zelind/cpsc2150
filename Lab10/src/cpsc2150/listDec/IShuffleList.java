20ded3e0d3c555301cd7d4ce722579d364857d24
zelindl <zelindl@g.clemson.edu>
2020-03-26 12:13:41 -0400

    added lab 10

---
+++ Lab10/src/cpsc2150/listDec/IShuffleList.java
@@ -0,0 +1,16 @@
+package cpsc2150.listDec;
+
+import java.util.*;
+
+/**
+ * Created by zelindl on 3/26/20.
+ */
+public interface IShuffleList extends List {
+    default void shuffle(int swaps){
+
+    }
+
+    default void swap(int i, int j){
+
+    }
+}
